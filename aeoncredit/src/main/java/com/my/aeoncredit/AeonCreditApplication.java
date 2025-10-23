806ac27635ab3fbb2fc4c5405e6530a70ac7c120
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2025-10-23 15:54:30 +0800

    project added

---
+++ aeoncredit/src/main/java/com/my/aeoncredit/AeonCreditApplication.java
@@ -0,0 +1,13 @@
+package com.my.aeoncredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeonCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeonCreditApplication.class, args);
+	}
+
+}
---
+++ aeoncredit/src/test/java/com/my/aeoncredit/AeonCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.aeoncredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeonCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
